983428a399fa29ce277cbc42d51e0b50ba9f3180
Niamh <niamh.gowran2@mail.dcu.ie>
2022-11-10 23:27:34 +0000

    nav

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
   "./pages/**/*.{js,ts,jsx,tsx}",
   "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkBlue: "#174484",
+      },
+    },
   },
   plugins: [],
 }
