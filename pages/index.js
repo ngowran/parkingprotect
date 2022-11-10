d8b52462b6e28a1eb77cb7cee6240367bf3250f4
Niamh <niamh.gowran2@mail.dcu.ie>
2022-11-10 22:29:23 +0000

    installs, brand colour, nav

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,13 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+
+import Nav from '../components/Nav'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Parking Protect</title>
-        <meta name="description" content="Parking Protect" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <div>
+  
+        <Nav/>
 
-      <main className={styles.main}>
-        
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Parking Protect{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+      
     </div>
   )
 }
