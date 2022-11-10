80470817a24b1b17c30e8a87fd83dfb08056ce53
Kevin James T <95762894+kmanjt@users.noreply.github.com>
2022-11-10 22:58:18 +0000

    assets, new favicon, font import

---
+++ components/Hero.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div className="w-full h-screen text-center">
+      <div className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center">
+        <div>
+          <h3 className="text-5xl tracking-widest">
+            Protecting People&apos;s Places
+          </h3>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 
+import Hero from '../components/Hero'
 import Nav from '../components/Nav'
 
 export default function Home() {
@@ -6,7 +7,7 @@ export default function Home() {
     <div>
   
         <Nav/>
-
+        <Hero/>
       
     </div>
   )
