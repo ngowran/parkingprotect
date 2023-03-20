763037b2f998af38bfbbb68bd244117fc7e9edab
Kevin James T <95762894+kmanjt@users.noreply.github.com>
2023-03-20 22:01:44 +0000

    Team section update

--- components/TeamData.js
+++ components/TeamData.js
@@ -3,6 +3,8 @@ import Kevin from "../public/team/Kevin.png";
 import Diarmuid from "../public/team/Diarmuid.png";
 import Niamh from "../public/team/Niamh.png";
 import Sadhana from "../public/team/Sadhana.png";
+import Sarah from "../public/team/Sarah.png";
+import Arath from "../public/team/Arath.png";
 
 export const TeamData = [
   {
@@ -23,6 +25,18 @@ export const TeamData = [
     name: "Maximilian Mohan",
     url: "https://www.linkedin.com/in/maximilian-mohan-218118252/",
     role: "Marketing Lead",
+  },
+  {
+    image: Sarah,
+    name: "Sarah Aliane",
+    url: "https://www.linkedin.com/in/sarah-aliane/",
+    role: "Business Analyst",
+  },
+  {
+    image: Arath,
+    name: "Arath Hernandez",
+    url: "https://www.linkedin.com/in/arath-hernandez-734b53215/",
+    role: "Business Analyst",
   },
   {
     image: Niamh,
